7e36c87e12ac668da0ea1a8b99008d726f15d87a
Your name <Melarajoaquin@gmail.com>
2016-10-29 13:28:12 -0400

    cdn

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,12 +13,16 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 //Routes
 
-app.get('/view', function (req, res) {
-	res.sendFile(path.join(__dirname, 'view.html'));
+app.get('/tables', function (req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'));
 });
 
-app.get('/make', function (req, res) {
-	res.sendFile(path.join(__dirname, 'make.html'));
+app.get('/reserve', function (req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/home', function (req, res) {
+	res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 app.get('/', function (req, res) {
